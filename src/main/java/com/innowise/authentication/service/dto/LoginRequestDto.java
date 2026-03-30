0d15715b6743c2f07e37eeb440e43c5275ac73e8
tabletka97 <khatkevichmatsvei@gmail.com>
2026-03-30 12:39:13 +0300

    add @NotBlank for LoginRequestDto

--- src/main/java/com/innowise/authentication/service/dto/LoginRequestDto.java
+++ src/main/java/com/innowise/authentication/service/dto/LoginRequestDto.java
@@ -1,7 +1,11 @@
 package com.innowise.authentication.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequestDto(
+        @NotBlank
         String email,
+        @NotBlank
         String password
 ) {
 }
